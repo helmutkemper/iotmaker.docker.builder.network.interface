99cd66f4f4004ad3193f6740a0b4a7343ccc6bf5
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-14 14:53:15 -0300

    documenting

--- typeContainerBuilderNetworkInterface.go
+++ typeContainerBuilderNetworkInterface.go
@@ -1,6 +1,5 @@
 package iotmaker_docker_builder_network_interface
 
-
 import (
 	"github.com/docker/docker/api/types/network"
 )
@@ -9,5 +8,6 @@ type ContainerBuilderNetworkInterface interface {
 	Init() (err error)
 	GetConfiguration() (networkConfiguration *network.NetworkingConfig, err error)
 	NetworkCreate(name, subnet, gateway string) (err error)
+	GetNetworkName() (name string)
 	Remove() (err error)
 }
